4f25b371379d3eb803cf46bf7cbe2811cb6f8bb1
HCHB <32193248+HCHB@users.noreply.github.com>
2019-04-08 11:22:00 +0200

    The number 5

---
+++ GreenhouseWeb/Services/Measurements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenhouseWeb.Services
+{
+    public class Measurements
+    {
+        public double internalTemperature { get; set; }
+        public double externalTemperature { get; set; }
+        public double humidity { get; set; }
+        public double waterlevel { get; set; }
+        
+        public Measurements(double internalTemperature, double externalTemperature, double humidity, double waterlevel)
+        {
+            this.internalTemperature = internalTemperature;
+            this.externalTemperature = externalTemperature;
+            this.humidity = humidity;
+            this.waterlevel = waterlevel;
+        }
+    }
+}
--- GreenhouseWeb/Services/Servicesfacade.cs
+++ GreenhouseWeb/Services/Servicesfacade.cs
@@ -7,5 +7,20 @@ namespace GreenhouseWeb.Services
 {
     public class Servicesfacade
     {
+
+        private LiveData liveData;
+
+        public Servicesfacade()
+        {
+            this.liveData = new LiveData();
+        }
+
+        public Measurements getLCurrentLiveData(String greenhouseID)
+        {
+            return this.liveData.getMeasurements(greenhouseID);
+        }
+
+
+
     }
 }
