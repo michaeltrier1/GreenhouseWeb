9f7a3505b8344dda520acb1d448f1097721a1280
Maltesj <malte_sj@hotmail.com>
2019-04-29 17:30:21 +0200

    all the changes

--- GreenhouseWeb/Models/Greenhouse.cs
+++ GreenhouseWeb/Models/Greenhouse.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GreenhouseWeb.Models
 {
     public class Greenhouse
     {
-
-        public int ID { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get;}
         public string GreenhouseID { get; set; }
         public string Password { get; set; }
         public string IP { get; set; }
--- GreenhouseWeb/Models/GreenhouseDBContext.cs
+++ GreenhouseWeb/Models/GreenhouseDBContext.cs
@@ -9,6 +9,7 @@ namespace GreenhouseWeb.Models
     public class GreenhouseDBContext : DbContext
     {
         public DbSet<Greenhouse> Greenhouses { get; set; }
+        public DbSet<Datalog> Datalogs { get; set; }
         public DbSet<Schedule> Schedules { get; set; }
     }
 }
