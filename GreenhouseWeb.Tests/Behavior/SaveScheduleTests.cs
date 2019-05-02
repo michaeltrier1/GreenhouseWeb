9ad8cf65829ca80a6b1863b8d614f5d689779aa0
HCHB <32193248+HCHB@users.noreply.github.com>
2019-05-02 13:51:06 +0200

    Test Skeleton

---
+++ GreenhouseWeb.Tests/Behavior/SaveScheduleTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GreenhouseWeb.Tests.Behavior
+{
+    /// <summary>
+    /// Summary description for SaveSchedule
+    /// </summary>
+    [TestClass]
+    public class SaveScheduleTests
+    {
+        public SaveScheduleTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ScheduleIsValid()
+        {
+            //
+            // TODO: Add test logic here
+            //
+
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void ScheduleIsInvalid()
+        {
+            //
+            // TODO: Add test logic here
+            //
+
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
